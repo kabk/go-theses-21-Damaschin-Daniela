d35672c869fc21b4b786d13adf3cb2666b6a45e4
Daniela510 <43034761+Daniela510@users.noreply.github.com>
2021-01-21 11:02:18 +0100

    Dynamic landing
    It moves the visiblilty window sometimes, idk yyy

--- scripppt.js
+++ scripppt.js
@@ -1,3 +1,41 @@
 $(".container").on("scroll", function () {
        $(".container").scrollTop($(this).scrollTop());
    });
+   let tl = anime.timeline({
+   });
+   tl.add({
+    targets: '#partl',
+    duration: 1000,
+    opacity: 1,
+    translateX: ['28vw', 0],
+    delay: 0,
+    easing: 'easeInOutQuart',
+}).add({
+ targets: '#parbl',
+ duration: 1000,
+ opacity: 1,
+ translateX: ['28vw', 0],
+ delay: -1000,
+ easing: 'easeInOutQuart',
+}).add({
+ targets: '#partr',
+ duration: 1000,
+ opacity: 1,
+ translateX: ['-28vw', 0],
+ delay: -1000,
+ easing: 'easeInOutQuart',
+}).add({
+targets: '#parbr',
+duration: 1000,
+opacity: 1,
+translateX: ['-28vw', 0],
+delay: -1000,
+easing: 'easeInOutQuart',
+}).add({
+targets: '.wrap',
+duration: 1500,
+opacity: 1,
+paddingTop: 0,
+delay: 0,
+easing: 'easeInOutQuart',
+})
